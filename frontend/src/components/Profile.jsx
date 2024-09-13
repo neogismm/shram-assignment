9de6020532d6460ad57384d4dedd252dfa074420
Pritam Neog <neog.geek123@gmail.com>
2024-09-14 02:10:48 +0530

    changed logout button styling in profile

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -4,10 +4,13 @@ export default function Profile() {
   const { user, logout } = useUser();
   return (
     <div className="text-center">
-      <h1 className="text-4xl font-bold mb-4">Profile</h1>
-      <p>Coming soon!</p>
-      {user && <p>User: {user.name}</p>}
-      <button onClick={logout}>Logout</button>
+      <h1 className="text-4xl font-bold mb-4">Hello {user.name}!</h1>
+      <button
+          onClick={logout}
+          className="bg-rose-600 hover:bg-rose-700 text-white font-bold py-2 px-4 rounded transition-colors"
+        >
+          Logout
+        </button>
     </div>
   );
 }
